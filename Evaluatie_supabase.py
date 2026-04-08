02a61728f060876df4e86b94a74662b3027efd1c
LeeShem <shem.vanderlee@gmail.com>
2026-04-08 20:56:12 +0200

    Update Evaluatie_supabase.py

--- Evaluatie_supabase.py
+++ Evaluatie_supabase.py
@@ -162,13 +162,13 @@ NIVEAU_BESCHRIJVING = {
 SCHAAL_INFO = """<div class="schaal-info">
 <h4>Info Beoordelingsschaal</h4>
 <p>Het is belangrijk om te beseffen dat de term <strong>neutraal</strong> in deze context niet
-gelijkstaat aan een voldoende, maar wordt geinterpreteerd als een <strong>gemiddelde prestatie
-(circa een 5)</strong>. Dit betekent dat een neutrale beoordeling nog als onvoldoende wordt beschouwd.</p>
+gelijkstaat aan een volle voldoende, maar wordt geinterpreteerd als een <strong>gemiddelde prestatie
+(circa een 5 á 6)</strong>. Dit betekent dat een neutrale beoordeling nog als onvoldoende/ net aan voldoende wordt beschouwd.</p>
 <p><strong>De beoordelingscategorieen zijn als volgt ingedeeld:</strong></p>
 <ul>
     <li><strong>Zeer slecht:</strong> 1 - 2,5</li>
     <li><strong>Slecht:</strong> 2,5 - 5</li>
-    <li><strong>Neutraal:</strong> 5 (onvoldoende)</li>
+    <li><strong>Neutraal:</strong> 5 - 6</li>
     <li><strong>Goed:</strong> 7 - 8,5</li>
     <li><strong>Excellent:</strong> 8,5 - 10</li>
 </ul>
